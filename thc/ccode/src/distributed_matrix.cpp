255a78222f86fb0b2814c3531cdaa5a3379401d8
Fionn Malone <f.malone13@imperial.ac.uk>
2018-03-21 17:47:45 -0700

    Distributed matrix helper class.
    Very basic matrix class to help distributing matrices between different
    blacs contexts.

---
+++ thc/ccode/src/distributed_matrix.cpp
@@ -0,0 +1,62 @@
+#include <vector>
+#include "distributed_matrix.h"
+#include "cblacs_defs.h"
+
+namespace DistributedMatrix
+{
+  // Constructor.
+  Matrix::Matrix(int m, int n, int block_m, int block_n, int ctxt, int root_ctxt)
+  {
+    nrows = m;
+    ncols = n;
+    block_nrows = block_m;
+    block_ncols = block_n;
+    // Offsets.
+    izero = 0;
+    init_row_idx = 1; // fortran indexing.
+    init_col_idx = 1;
+    // Query location in processor grid.
+    Cblacs_gridinfo(ctxt, &proc_nrows, &proc_ncols, &proc_row, &proc_col);
+    local_nrows = numroc_(&nrows, &block_nrows, &proc_row, &izero, &proc_nrows); 
+    local_ncols = numroc_(&ncols, &block_ncols, &proc_col, &izero, &proc_ncols); 
+    // Local leading dimension.
+    lld = std::max(1, local_nrows);
+    desc_global.resize(9);
+    desc_local.resize(9);
+    // Initialise local discriptor array.
+    int irsrc = 0, icsrc = 0;
+    descinit_(desc_local.data(), &nrows, &ncols, &block_nrows,
+              &block_ncols, &irsrc, &icsrc, &ctxt, &lld, &info);
+    // Initialise global descriptor array for array stored on root node.
+    // We fake a 1x1 processor grid.
+    desc_global[1] = -1;
+    if (proc_row == 0 && proc_col == 0) {
+      descinit_(desc_global.data(), &nrows, &ncols, &nrows,
+                &ncols, &irsrc, &icsrc, &root_ctxt, &nrows, &info);
+    }
+    // Allocate memory.
+    global_data.resize(nrows*ncols);
+    local_data.resize(local_nrows*local_ncols);
+  }
+  // Scatter global matrix from root processor to childred in block cyclic distribution.
+  void Matrix::scatter_block_cyclic(int ctxt)
+  {
+    pdgemr2d_(&nrows, &ncols,
+              global_data.data(), &init_row_idx, &init_col_idx, desc_global.data(),
+              local_data.data(), &init_row_idx, &init_col_idx, desc_local.data(),
+              &ctxt);
+  }
+  // Gather block-cyclically distributed matrix to master processor/node. 
+  void Matrix::gather_block_cyclic(int ctxt)
+  {
+    pdgemr2d_(&nrows, &ncols,
+              local_data.data(), &init_row_idx, &init_row_idx, desc_local.data(),
+              global_data.data(), &init_row_idx, &init_col_idx, desc_global.data(),
+              &ctxt);
+  }
+  // Destructor.
+  // Will need to deal with releasing memory.
+  Matrix::~Matrix()
+  {
+  }
+}
---
+++ thc/ccode/src/distributed_matrix.h
@@ -0,0 +1,45 @@
+#ifndef DISTRIBUTED_MATRIX_H
+#define DISTRIBUTED_MATRIX_H 
+#include <iostream>
+#include <vector>
+
+// Operations for matrices distributed using scalapack.
+// Asumes column major (Fortran) order for layout.
+// Thin wrapper to distribute data to and from block cyclic form.
+namespace DistributedMatrix
+{
+  class Matrix
+  {
+    public:
+      Matrix(int nrows, int ncols, int block_rows, int block_cols, int ctxt, int root_ctxt);
+      ~Matrix();
+      void gather_block_cyclic(int ctxt);
+      void scatter_block_cyclic(int ctxt);
+      // global matrix dimensions
+      int nrows;
+      int ncols;
+      // Block sizes.
+      int block_nrows;
+      int block_ncols;
+      // processor grid.
+      int proc_nrows;
+      int proc_ncols;
+      // Location in processor grid. 
+      int proc_row;
+      int proc_col;
+      // local number of rows and columns.
+      int local_nrows;
+      int local_ncols;
+      // local leading dimension.
+      int lld;
+      // Variables for fancy selection of data. Not used set to 0 or 1.
+      int izero;
+      int init_row_idx, init_col_idx;
+      // Variable for fortran interface.
+      int info;
+      // Global and local data stores for matrix.
+      std::vector<double> global_data, local_data;
+      std::vector<int> desc_global, desc_local;
+  };
+}
+#endif
