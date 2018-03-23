adbac4fe2c18b278768f628592a29caebe693be5
Fionn Malone <f.malone13@imperial.ac.uk>
2018-03-23 14:58:17 -0700

    Hacky fft.

--- thc/ccode/src/main.cpp
+++ thc/ccode/src/main.cpp
@@ -2,11 +2,14 @@
 #include <vector>
 #include <iomanip>
 #include <time.h>
+#include <complex>
+#include <fftw3.h>
 #include <mpi.h>
-#include "h5helper.h"
-#include "matrix_operations.h"
 #include "cblacs_defs.h"
-#include "fftw3.h"
+#include "distributed_matrix.h"
+#include "matrix_operations.h"
+#include "h5helper.h"
+#include "utils.h"
 
 int main(int argc, char* argv[])
 {
@@ -69,7 +72,38 @@ int main(int argc, char* argv[])
   }
   DistributedMatrix::Matrix CZ(CZt.ncols, CZt.nrows, block_rows, block_cols, ctxt, root_ctxt, ccyc_ctxt);
   MatrixOperations::transpose(CZt, CZ);
-  if (root) CZ.global_data.resize(CZ.nrows*CZ.ncols);
-  CZ.gather_block_cyclic(ctxt);
+  // will now have matrix in C order (nmu/nprocs, ngs)
+  CZ.redistribute_to_column_cyclic(ctxt);
+  // FFT interpolating vectors
+  if (root) {
+    std::cout << "Performing FFT of interpolating functions." << std::endl;
+    std::cout << "DIMS: " << CZ.nrows << " " << CZ.ncols << " " << CZ.ccyc_nrows << " " << CZ.ccyc_ncols << std::endl;
+  }
+  fftw_plan p;
+  int ngs = CZ.nrows;
+  int ng = (int)pow(ngs, 1.0/3.0);
+  int offset = ngs;
+  if (root) {
+    std::cout << "offset between grid points: " << offset << std::endl;
+    std::cout << "ngs: " << ng << std::endl;
+  }
+  for (int i = 0; i < CZ.ccyc_ncols; i++) {
+    std::vector<std::complex<double> > complex_data = UTILS::convert_double_to_complex(CZ.ccyc_data.data()+i*offset, ngs);
+    if (root) {
+      std::cout << "Performing FFT " << i+1 << " of " <<  CZ.ccyc_ncols << " " << i*offset << " " << complex_data.size() << std::endl;
+    }
+    p = fftw_plan_dft_3d(ng, ng, ng, reinterpret_cast<fftw_complex*> (complex_data.data()),
+                         reinterpret_cast<fftw_complex*>(CZ.fft_data.data()+i*offset),
+                         FFTW_FORWARD, FFTW_ESTIMATE);
+    fftw_execute(p);
+    fftw_destroy_plan(p);
+  }
+  CZ.gather_fft(ccyc_ctxt);
+  if (root) {
+    // The data which was in FTN order has been transposed, but we want to print in C
+    // order, so reverse dimensions.
+    H5Helper::write_fft(CZ.cglobal_data, CZ.ncols, CZ.nrows);
+  }
+  //rfftwnd_one_real_to_complex(p, CZ.ccyc_data.data(), CZ.fft_data.data());
   MPI_Finalize();
 }
