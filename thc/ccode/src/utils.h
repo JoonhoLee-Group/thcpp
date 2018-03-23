f86dece3a725bdf9b5d350ef3209b5b9e9aa4781
Fionn Malone <f.malone13@imperial.ac.uk>
2018-03-23 14:55:59 -0700

    Convert vector of doubles to vector of complex.

--- thc/ccode/src/utils.h
+++ thc/ccode/src/utils.h
@@ -9,5 +9,17 @@ inline double get_memory(std::vector<double> &vec)
   return vec.size() * (double)sizeof(double) / 1000 / 1000 / 1000;
 }
 
+inline std::vector<std::complex< double> > convert_double_to_complex(double *data, int size)
+{
+  std::vector<std::complex<double> > cdata(size);
+  double *d = data;
+  for (int i = 0; i < cdata.size(); i++) {
+    cdata[i].real() = *d;
+    cdata[i].imag() = 0.0;
+    d++;
+  }
+  return cdata;
+}
+
 }
 #endif
