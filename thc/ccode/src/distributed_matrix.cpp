fd0ea1ed0020ce45c87720bcb0b3b8269a6093aa
Fionn Malone <f.malone13@imperial.ac.uk>
2018-03-31 15:37:50 -0700

    Refactor distributed matrix to use blacs objects.
    Make a bit more cleaner by removing specialised routines in favour of
    generic redistribution routine given two different blacs objects.
    
    Not tested.

--- thc/ccode/src/distributed_matrix.cpp
+++ thc/ccode/src/distributed_matrix.cpp
@@ -2,34 +2,35 @@
 #include <mpi.h>
 #include <time.h>
 #include "distributed_matrix.h"
-#include "cblacs_defs.h"
 #include "scalapack_defs.h"
 #include "H5Cpp.h"
 #include "h5helper.h"
 #include "utils.h"
+#include "context_handler.h"
 
 namespace DistributedMatrix
 {
   // Constructor without having data.
-  Matrix::Matrix(int m, int n, int block_m, int block_n, int &ctxt, int &root_ctxt, int &ccyc_ctxt)
+  Matrix::Matrix(int m, int n, ContextHandler::BlacsGrid &Grid)
   {
     nrows = m;
     ncols = n;
-    block_nrows = block_m;
-    block_ncols = block_n;
+    // Hardcoded for now.
+    block_nrows = 64;
+    block_ncols = 64;
     // Offsets.
     izero = 0;
     init_row_idx = 1; // fortran indexing.
     init_col_idx = 1;
     // Setup descriptor arrays for block cyclic distribution.
-    initialise_discriptors(ctxt, root_ctxt, ccyc_ctxt);
+    desc.resize(9);
+    initialise_discriptor(desc, Grid, local_nrows, local_ncols);
     // Allocate memory.
-    local_data.resize(local_nrows*local_ncols);
-    ccyc_data.resize(nrows*ccyc_ncols);
-    fft_data.resize(nrows*ccyc_ncols);
+    store.resize(local_nrows*local_ncols);
   }
-  Matrix::Matrix(std::string filename, std::string name, int block_m, int block_n,
-                 int &ctxt, int &root_ctxt, int &ccyc_ctxt, int rank)
+
+  Matrix::Matrix(std::string filename, std::string name,
+                 ContextHandler::BlacsGrid &Grid, int rank)
   {
     std::vector<hsize_t> dims(2);
     if (rank == 0) {
@@ -37,10 +38,10 @@ namespace DistributedMatrix
       double tread = clock();
       H5::H5File file = H5::H5File(filename, H5F_ACC_RDONLY);
       // Read data from file.
-      H5Helper::read_matrix(file, name, global_data, dims);
+      H5Helper::read_matrix(file, name, store, dims);
       tread = clock() - tread;
       std::cout << "Time taken to read matrix: " << " " << tread / CLOCKS_PER_SEC << " seconds" << std::endl;
-      double memory = UTILS::get_memory(global_data);
+      double memory = UTILS::get_memory(store);
       std::cout << "Memory usage for " << name << ": " << memory << " GB" << std::endl;
       std::cout << "Assuming matrices are in FORTRAN / column major format." << std::endl;
       std::cout << "Matrix shape: (" << dims[1] << ", " << dims[0] << ")" << std::endl;
@@ -51,87 +52,66 @@ namespace DistributedMatrix
     // to swap dimensions for rows and columns.
     nrows = dims[1];
     ncols = dims[0];
-    block_nrows = block_m;
-    block_ncols = block_n;
+    // Hardcoded.
+    block_nrows = 64;
+    block_ncols = 64;
     // Offsets.
     izero = 0;
     init_row_idx = 1; // fortran indexing.
     init_col_idx = 1;
     // Setup descriptor arrays for block cyclic distribution.
-    initialise_discriptors(ctxt, root_ctxt, ccyc_ctxt);
-    // Store for local arrays.
-    local_data.resize(local_nrows*local_ncols);
-    ccyc_data.resize(nrows*ccyc_ncols);
-    if (rank == 0) {
-      double memory = UTILS::get_memory(local_data);
+    desc.resize(9);
+    initialise_discriptor(desc, Grid, local_nrows, local_ncols);
+  }
+  void Matrix::initialise_discriptor(std::vector<int> &desc, ContextHandler::BlacsGrid &Grid, int &local_nr, int &local_nc)
+  {
+    local_nr = numroc_(&nrows, &block_nrows, &Grid.row,
+                       &izero, &Grid.nrows);
+    local_nc = numroc_(&ncols, &block_ncols, &Grid.col, &izero,
+                       &Grid.ncols);
+    lld = std::max(1, local_nr);
+    int irsrc = 0, icsrc = 0;
+    // 1x1 grid.
+    if (Grid.nrows == 1 && Grid.ncols == 1) {
+      if (Grid.row == 0 && Grid.col == 1) {
+        descinit_(desc.data(), &nrows, &ncols, &block_nrows,
+                  &block_ncols, &irsrc, &icsrc, &Grid.ctxt, &lld,
+                  &info);
+      } else {
+        desc[1] = -1;
+      }
+    } else {
+      descinit_(desc.data(), &nrows, &ncols, &block_nrows,
+                &block_ncols, &irsrc, &icsrc, &Grid.ctxt, &lld,
+                &info);
+    }
+  }
+  void Matrix::redistribute(ContextHandler::BlacsGrid &GridA, ContextHandler::BlacsGrid &GridB)
+  {
+    // setup discriptor for Blacs grid we'll distribute to.
+    std::vector<int> descb(9);
+    int nr, nc;
+    initialise_discriptor(descb, GridB, nr, nc);
+    std::vector<double> tmp(nr*nc);
+    int ctxt;
+    // ctxt for p?gemr2d call must at least contain the union of processors from gridA and
+    // gridB.
+    if (GridA.nrows*GridA.ncols >= GridB.nrows*GridB.ncols) {
+      ctxt = GridA.ctxt;
+    } else {
+      ctxt = GridB.ctxt;
+    }
+    pdgemr2d_(&nrows, &ncols,
+              store.data(), &init_row_idx, &init_row_idx, desc.data(),
+              tmp.data(), &init_row_idx, &init_col_idx, descb.data(),
+              &ctxt);
+    tmp.swap(store);
+#ifdef DEBUG
+    if (GridB.rank == 0) {
+      double memory = UTILS::get_memory(store);
       std::cout << "Local memory usage (on root processor) for " << name << ": " << memory << " GB" << std::endl;
     }
-  }
-  // Initialise descriptor arrays for block cyclic distributions.
-  void Matrix::initialise_discriptors(int ctxt, int root_ctxt, int ccyc_ctxt)
-  {
-    // Query location in processor grid.
-    Cblacs_gridinfo(ctxt, &proc_nrows, &proc_ncols, &proc_row, &proc_col);
-    local_nrows = numroc_(&nrows, &block_nrows, &proc_row, &izero, &proc_nrows);
-    local_ncols = numroc_(&ncols, &block_ncols, &proc_col, &izero, &proc_ncols);
-    // Local leading dimension.
-    lld = std::max(1, local_nrows);
-    desc_global.resize(9);
-    desc_local.resize(9);
-    desc_ccyc.resize(9);
-    // Initialise local discriptor array.
-    int irsrc = 0, icsrc = 0;
-    descinit_(desc_local.data(), &nrows, &ncols, &block_nrows,
-              &block_ncols, &irsrc, &icsrc, &ctxt, &lld, &info);
-    // Initialise global descriptor array for array stored on root node.
-    if (proc_row == 0 && proc_col == 0) {
-      descinit_(desc_global.data(), &nrows, &ncols, &nrows,
-                &ncols, &irsrc, &icsrc, &root_ctxt, &nrows, &info);
-    } else {
-      // This needs to be set to -1 on processors other than the root processor
-      // for redistribution to work.
-      desc_global[1] = -1;
-    }
-    // column cyclic distribution.
-    int ncols_per_block = 1;
-    int ccyc_proc_nrows, ccyc_proc_ncols, ccyc_proc_row, ccyc_proc_col;
-    Cblacs_gridinfo(ccyc_ctxt, &ccyc_proc_nrows, &ccyc_proc_ncols, &ccyc_proc_row, &ccyc_proc_col);
-    ccyc_ncols = numroc_(&ncols, &ncols_per_block, &ccyc_proc_row, &izero, &ccyc_proc_ncols);
-    ccyc_nrows = numroc_(&nrows, &nrows, &ccyc_proc_row, &izero, &ccyc_proc_nrows);
-    descinit_(desc_ccyc.data(), &nrows, &ncols, &nrows, &ccyc_ncols,
-              &irsrc, &icsrc, &ccyc_ctxt, &nrows, &info);
-
-  };
-  // Scatter global matrix from root processor to childred in block cyclic distribution.
-  void Matrix::scatter_block_cyclic(int ctxt)
-  {
-    pdgemr2d_(&nrows, &ncols,
-              global_data.data(), &init_row_idx, &init_col_idx, desc_global.data(),
-              local_data.data(), &init_row_idx, &init_col_idx, desc_local.data(),
-              &ctxt);
-  }
-  // Gather block-cyclically distributed matrix to master processor/node.
-  void Matrix::gather_block_cyclic(int ctxt)
-  {
-    pdgemr2d_(&nrows, &ncols,
-              local_data.data(), &init_row_idx, &init_row_idx, desc_local.data(),
-              global_data.data(), &init_row_idx, &init_col_idx, desc_global.data(),
-              &ctxt);
-  }
-  void Matrix::redistribute_to_column_cyclic(int ctxt)
-  {
-    pdgemr2d_(&nrows, &ncols,
-              local_data.data(), &init_row_idx, &init_row_idx, desc_local.data(),
-              ccyc_data.data(), &init_row_idx, &init_col_idx, desc_ccyc.data(),
-              &ctxt);
-  }
-  void Matrix::gather_fft(int ctxt)
-  {
-    if (cglobal_data.size() == 0) cglobal_data.resize(nrows*ncols);
-    pzgemr2d_(&nrows, &ncols,
-              fft_data.data(), &init_row_idx, &init_row_idx, desc_ccyc.data(),
-              cglobal_data.data(), &init_row_idx, &init_col_idx, desc_global.data(),
-              &ctxt);
+#endif
   }
   // Destructor.
   // Will need to deal with releasing memory.
--- thc/ccode/src/distributed_matrix.h
+++ thc/ccode/src/distributed_matrix.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <complex>
+#include "context_handler.h"
 
 // Operations for matrices distributed using scalapack.
 // Asumes column major (Fortran) order for layout.
@@ -12,26 +13,23 @@ namespace DistributedMatrix
   class Matrix
   {
     public:
-      Matrix(int nrows, int ncols, int block_rows, int block_cols, int &ctxt, int &root_ctxt, int &ccyc_ctxt);
-      Matrix(std::string filename, std::string name, int block_rows, int block_cols, int &ctxt, int &root_ctxt, int &ccyc_ctxt, int rank);
+      Matrix(int nrows, int ncols, ContextHandler::BlacsGrid &Grid);
+      Matrix(std::string filename, std::string name,
+             ContextHandler::BlacsGrid &Grid, int rank);
       ~Matrix();
       void gather_block_cyclic(int ctxt);
       void gather_fft(int ctxt);
       void scatter_block_cyclic(int ctxt);
+      void redistribute(ContextHandler::BlacsGrid &GridA, ContextHandler::BlacsGrid &GridB);
       void redistribute_to_column_cyclic(int ctxt);
       void initialise_discriptors(int ctxt, int root_ctxt, int ccyc_ctxt);
+      void initialise_discriptor(std::vector<int> &desc, ContextHandler::BlacsGrid &Grid, int &nr, int &nc);
       // global matrix dimensions
       int nrows;
       int ncols;
       // Block sizes.
       int block_nrows;
       int block_ncols;
-      // processor grid.
-      int proc_nrows;
-      int proc_ncols;
-      // Location in processor grid.
-      int proc_row;
-      int proc_col;
       // local number of rows and columns.
       int local_nrows;
       int local_ncols;
@@ -45,9 +43,9 @@ namespace DistributedMatrix
       // Variable for fortran interface.
       int info;
       // Global and local data stores for matrix.
-      std::vector<double> global_data, local_data, ccyc_data;
-      std::vector<int> desc_global, desc_local, desc_ccyc;
-      std::vector<std::complex<double> > fft_data, cglobal_data;
+      std::vector<double> store;
+      // scalapack descriptor array for current blacs context.
+      std::vector<int> desc;
   };
 }
 #endif
