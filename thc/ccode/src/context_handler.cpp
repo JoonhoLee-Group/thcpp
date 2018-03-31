d7b80425eb81fe5f417b6e6450756db8913a08f2
Fionn Malone <f.malone13@imperial.ac.uk>
2018-03-31 15:37:35 -0700

    Add blacs context objects.

---
+++ thc/ccode/src/context_handler.cpp
@@ -0,0 +1,31 @@
+#include "context_handler.h"
+#include <math.h>
+#include <iostream>
+
+namespace ContextHandler
+{
+  BlacsHandler::BlacsHandler()
+  {
+    Cblacs_pinfo(&rank, &nprocs);
+    proc_rows = (int)sqrt(nprocs);
+    proc_cols = nprocs / proc_rows;
+    if (proc_rows != proc_cols) {
+      std::cout << "Not running on square processor grid." << std::endl;
+    }
+    // Initialise differenct blacs grids.
+    Root = BlacsGrid(1, 1);
+    Square = BlacsGrid(proc_rows, proc_cols);
+    Column = BlacsGrid(1, proc_rows*proc_cols);
+  }
+  // Constructor for potentially non-square processor grid.
+  BlacsGrid::BlacsGrid(int nr, int nc)
+  {
+    nrows = nr;
+    ncols = nc;
+    int ctxt_sys;
+    Cblacs_get(0, 0, &ctxt_sys);
+    ctxt = ctxt_sys;
+    Cblacs_gridinit(&ctxt, "Row-major", nrows, ncols);
+    Cblacs_gridinfo(ctxt, &nrows, &ncols, &row, &col);
+  }
+}
---
+++ thc/ccode/src/context_handler.h
@@ -0,0 +1,29 @@
+#ifndef CONTEXT_HANDLER_H
+#define CONTEXT_HANDLER_H
+#include "cblacs_defs.h"
+
+namespace ContextHandler
+{
+  class BlacsGrid
+  {
+      public:
+        BlacsGrid() {};
+        BlacsGrid(int nr, int nc);
+        int nrows, ncols;
+        int row, col;
+        int ctxt;
+  };
+  // Handle different blacs grid distributions
+  class BlacsHandler
+  {
+    public:
+      BlacsHandler();
+      ~BlacsHandler();
+      int rank;
+      int nprocs, proc_rows, proc_cols;
+      BlacsGrid Root;
+      BlacsGrid Square;
+      BlacsGrid Column;
+  };
+}
+#endif
