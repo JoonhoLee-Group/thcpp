99bc9a30b0bb1e50720859e0256b25b22fd60071
Fionn Malone <f.malone13@imperial.ac.uk>
2018-04-03 12:43:31 -0700

    Fix construction of matrices.

--- thc/ccode/src/interpolating_vectors.cpp
+++ thc/ccode/src/interpolating_vectors.cpp
@@ -32,7 +32,6 @@ namespace InterpolatingVectors
     // Done on single processor.
     aoR.initialise_discriptor(aoR.desc, BH.Root, aoR.local_nrows, aoR.local_ncols);
     // Actually do need to transpose aoR_mu's data.
-    DistributedMatrix::Matrix aoR_mu_T(aoR_mu.ncols, aoR_mu.nrows, BH.Root);
     if (BH.rank == 0) {
       std::vector<double> tmp(aoR_mu.store.size());
       for (int i = 0; i < aoR_mu.nrows; ++i) {
@@ -44,26 +43,30 @@ namespace InterpolatingVectors
     }
     //MPI_Barrier(MPI_COMM_WORLD);
     // But shape (Nmu, M) should stay the same.
-    tmp = aoR_mu_T.nrows;
-    aoR_mu_T.nrows = aoR_mu_T.ncols;
-    aoR_mu_T.ncols = tmp;
-    aoR_mu_T.initialise_discriptor(aoR_mu_T.desc, BH.Root, aoR_mu_T.local_ncols, aoR_mu_T.local_nrows);
+    //tmp = aoR_mu.nrows;
+    //aoR_mu.nrows = aoR_mu.ncols;
+    //aoR_mu.ncols = tmp;
+    //aoR_mu.initialise_discriptor(aoR_mu_T.desc, BH.Root, aoR_mu_T.local_ncols, aoR_mu_T.local_nrows);
     // Finally construct CZt.
     if (BH.rank == 0) {
       std::cout << " * Constructing CZt matrix" << std::endl;
     }
-    CZt.setup_matrix(aoR_mu_T.nrows, aoR.ncols, BH.Root);
+    CZt.setup_matrix(aoR_mu.nrows, aoR.ncols, BH.Root);
     CCt.setup_matrix(CZt.nrows, interp_indxs.size(), BH.Root);
     if (BH.rank == 0) {
-      MatrixOperations::product(aoR_mu_T, aoR, CZt);
+      MatrixOperations::product(aoR_mu, aoR, CZt);
       // Hadamard products.
       for (int i = 0; i < CZt.store.size(); ++i) {
         CZt.store[i] *= CZt.store[i];
       }
+      //std::cout << MatrixOperations::vector_sum(CZt.store) << std::endl;
+      std::cout << " * Matrix Shape: (" << CZt.nrows << ", " << CZt.ncols << ")" << std::endl;
       std::cout << " * Constructing CCt matrix" << std::endl;
+      std::cout << " * Matrix Shape: (" << CCt.nrows << ", " << CCt.ncols << ")" << std::endl;
       // Need to down sample columns of CZt to form CCt, both of which have their data store
       // in fortran / column major order.
       MatrixOperations::down_sample(CZt, CCt, interp_indxs, CZt.nrows);
+      //std::cout << MatrixOperations::vector_sum(CCt.store) << std::endl;
     }
     // Block cyclically distribute.
     if (BH.rank == 0) {
@@ -87,8 +90,10 @@ namespace InterpolatingVectors
     }
     double tlsq = clock();
     MatrixOperations::least_squares(CCt, CZt);
+    CZt.redistribute(BH.Square, BH.Root);
     tlsq = clock() - tlsq;
     if (BH.rank == 0) {
+      //std::cout << MatrixOperations::vector_sum(CZt.store) << std::endl;
       std::cout << " * Time for least squares solve : " << tlsq / CLOCKS_PER_SEC << " seconds" << std::endl;
       std::cout << std::endl;
     }
