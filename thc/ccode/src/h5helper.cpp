54e860551cf36ee665eb847bd5b48571e249980e
Fionn Malone <f.malone13@imperial.ac.uk>
2018-04-05 14:04:31 -0700

    Template / overload matrix reading.

--- thc/ccode/src/h5helper.cpp
+++ thc/ccode/src/h5helper.cpp
@@ -5,39 +5,20 @@
 
 namespace H5Helper
 {
-  void read_matrix(H5::H5File matrix_file, const H5std_string data_name,
-                    std::vector<double> &matrix, std::vector<hsize_t> &dims)
+  void read_dataset(H5::DataSet &dset, std::vector<double> &matrix, H5::DataSpace mspace, H5::DataSpace &dspace)
   {
-    const int ndims = 2;
-
-    // get signal dataset
-    H5::DataSet dataset = matrix_file.openDataSet(data_name);
-
-    // check that signal is float
-    if (dataset.getTypeClass() != H5T_FLOAT) {
-      std::cerr << "signal dataset has wrong type" << std::endl;
-    }
-
-    // check that signal is double
-    if (dataset.getFloatType().getSize() != sizeof(double)) {
-      std::cerr << "signal dataset has wrong type size" << std::endl;
-    }
-
-    // get the dataspace
-    H5::DataSpace dataspace = dataset.getSpace();
-    // check that signal has 2 dims
-    if (dataspace.getSimpleExtentNdims() != ndims) {
-      std::cerr << "signal dataset has wrong number of dimensions" << std::endl;
-    }
-
-    // get dimensions
-    dataspace.getSimpleExtentDims(dims.data(), NULL);
-
-    // allocate memory and read data
-    matrix.resize(dims[0]*dims[1]);
-
-    H5::DataSpace data_mspace(ndims, dims.data());
-    dataset.read(matrix.data(), H5::PredType::NATIVE_DOUBLE, data_mspace, dataspace);
+    dset.read(matrix.data(), H5::PredType::NATIVE_DOUBLE, mspace, dspace);
+  }
+  void read_dataset(H5::DataSet &dset, std::vector<int> &matrix, H5::DataSpace mspace, H5::DataSpace &dspace)
+  {
+    dset.read(matrix.data(), H5::PredType::NATIVE_INT, mspace, dspace);
+  }
+  void read_dataset(H5::DataSet &dset, std::vector<std::complex<double> > &matrix, H5::DataSpace mspace, H5::DataSpace &dspace)
+  {
+    H5::CompType complex_data_type(sizeof(matrix[0]));
+    complex_data_type.insertMember("r", 0, H5::PredType::NATIVE_DOUBLE);
+    complex_data_type.insertMember("i", sizeof(double), H5::PredType::NATIVE_DOUBLE);
+    dset.read(matrix.data(), complex_data_type, mspace, dspace);
   }
 
   void write(H5::H5File &fh5, std::string name, std::vector<double> &data, std::vector<hsize_t> &dims)
--- thc/ccode/src/h5helper.h
+++ thc/ccode/src/h5helper.h
@@ -5,7 +5,34 @@
 #include <complex>
 namespace H5Helper
 {
-  void read_matrix(H5::H5File matrix_file, const H5std_string data_name, std::vector<double> &matrix, std::vector<hsize_t> &dims);
+  void read_dataset(H5::DataSet &dset, std::vector<double> &matrix, H5::DataSpace mspace, H5::DataSpace &dspace);
+  void read_dataset(H5::DataSet &dset, std::vector<std::complex<double> > &matrix, H5::DataSpace mspace, H5::DataSpace &dspace);
+  void read_dataset(H5::DataSet &dset, std::vector<int> &matrix, H5::DataSpace mspace, H5::DataSpace &dspace);
+  template <typename T>
+  void read_matrix(H5::H5File matrix_file, const H5std_string data_name,
+                    std::vector<T> &matrix, std::vector<hsize_t> &dims)
+  {
+    const int ndims = 2;
+
+    // get dataset
+    H5::DataSet dataset = matrix_file.openDataSet(data_name);
+    // get the dataspace
+    H5::DataSpace dataspace = dataset.getSpace();
+    // check that signal has 2 dims
+    if (dataspace.getSimpleExtentNdims() != ndims) {
+      std::cerr << "dataset has wrong number of dimensions" << std::endl;
+    }
+
+    // get dimensions
+    dataspace.getSimpleExtentDims(dims.data(), NULL);
+
+    // allocate memory and read data
+    matrix.resize(dims[0]*dims[1]);
+
+    H5::DataSpace data_mspace(ndims, dims.data());
+    //dataset.read(matrix.data(), H5::PredType::NATIVE_DOUBLE, data_mspace, dataspace);
+    read_dataset(dataset, matrix, data_mspace, dataspace);
+  }
   void write(H5::H5File &fh5, std::string name, std::vector<std::complex<double> > &data, std::vector<hsize_t> &dims);
   void write(H5::H5File &fh5, std::string name, std::vector<double> &data, std::vector<hsize_t> &dims);
 }
