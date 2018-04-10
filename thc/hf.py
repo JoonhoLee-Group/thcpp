283ac66c36380353b75fd241a4a27a54557c9f52
Fionn Malone <f.malone13@imperial.ac.uk>
2018-04-09 17:45:42 -0700

    Orthogonalised basis HF energy script.

--- thc/hf.py
+++ thc/hf.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
-from thc.transform_basis import compute_thc_hf_energy
+from thc.transform_basis import compute_thc_hf_energy, compute_thc_hf_energy_wfn
 import sys
 
-ehf, ehf_ref, fock = compute_thc_hf_energy(sys.argv[1], sys.argv[2])
+if sys.argv[3] == "True":
+    print ("Computing HF energy in orthogonalised AO basis.")
+    ehf, ehf_ref = compute_thc_hf_energy_wfn(sys.argv[1], sys.argv[2])
+else:
+    print ("Computing HF energy in non-orthogonal AO basis.")
+    ehf, ehf_ref, fock = compute_thc_hf_energy(sys.argv[1], sys.argv[2])
 print (ehf, ehf_ref)
