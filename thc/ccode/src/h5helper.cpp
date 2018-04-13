bef042f71231720605c454803fa762698910151f
Fionn Malone <f.malone13@imperial.ac.uk>
2018-04-13 11:15:48 -0700

    Change format of complex numbers for QMCPACK.

--- thc/ccode/src/h5helper.cpp
+++ thc/ccode/src/h5helper.cpp
@@ -5,16 +5,20 @@
 
 namespace H5Helper
 {
+
   void read_dataset(H5::DataSet &dset, std::vector<double> &matrix, H5::DataSpace mspace, H5::DataSpace &dspace)
   {
     dset.read(matrix.data(), H5::PredType::NATIVE_DOUBLE, mspace, dspace);
   }
+
   void read_dataset(H5::DataSet &dset, std::vector<int> &matrix, H5::DataSpace mspace, H5::DataSpace &dspace)
   {
     dset.read(matrix.data(), H5::PredType::NATIVE_INT, mspace, dspace);
   }
+
   void read_dataset(H5::DataSet &dset, std::vector<std::complex<double> > &matrix, H5::DataSpace mspace, H5::DataSpace &dspace)
   {
+    // h5py format for complex numbers
     H5::CompType complex_data_type(sizeof(matrix[0]));
     complex_data_type.insertMember("r", 0, H5::PredType::NATIVE_DOUBLE);
     complex_data_type.insertMember("i", sizeof(double), H5::PredType::NATIVE_DOUBLE);
@@ -41,17 +45,15 @@ namespace H5Helper
 
   void write(H5::H5File &fh5, std::string dset_name, std::vector<std::complex<double> > &data, std::vector<hsize_t> &dims)
   {
-    // Create appropriate data type.
-    H5::CompType complex_data_type(sizeof(data[0]));
-    complex_data_type.insertMember("r", 0, H5::PredType::NATIVE_DOUBLE);
-    complex_data_type.insertMember("i", sizeof(double), H5::PredType::NATIVE_DOUBLE);
-    // Write data.
+    // QMCPACK complex number format.
+    dims.push_back(2);
     H5::DataSpace dataspace(dims.size(), dims.data());
     H5::DataSet dataset = fh5.createDataSet(dset_name.c_str(),
-                                             complex_data_type,
-                                             dataspace);
-    dataset.write(data.data(), complex_data_type);
+                                            H5::PredType::NATIVE_DOUBLE,
+                                            dataspace);
+    dataset.write(data.data(), H5::PredType::NATIVE_DOUBLE);
   }
+
   void read_dims(std::string filename, std::string data_name, std::vector<hsize_t> &dims)
   {
     H5::H5File file = H5::H5File(filename.c_str(), H5F_ACC_RDONLY);
