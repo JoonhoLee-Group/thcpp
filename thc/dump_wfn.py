56cc4e5d22f02572388e6ff66494ebbebcff172d
Fionn Malone <f.malone13@imperial.ac.uk>
2018-04-25 22:04:44 -0700

    Update wavefunction dump.

--- thc/dump_wfn.py
+++ thc/dump_wfn.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
-from thc.transform_basis import dump_thc_data
+from thc.transform_basis import dump_thc_data, dump_thc_data_sc
 import sys
 
-if sys.argv[2] == 'True':
+if sys.argv[2] == 'ortho':
     ortho = True
+    mos =  False
+elif sys.argv[2] == 'mos':
+    mos = True
+    ortho = False
 else:
     ortho = False
-dump_thc_data(sys.argv[1], ortho_ao=ortho, ao_file="aos.h5")
+    mos = False
+if sys.argv[3] == 'supercell':
+    dump_thc_data_sc(sys.argv[1], ortho_ao=ortho,
+                     orbital_file="aos.h5", wfn_file="wfn_thc.dat")
+else:
+    dump_thc_data(sys.argv[1], mos=mos, ortho_ao=ortho, orbital_file="aos.h5",
+                  wfn_file="wfn_thc.dat")
