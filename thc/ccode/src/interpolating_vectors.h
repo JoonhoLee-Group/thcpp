884ea382a4c1fc7d0f1496b5e92292cf155942ff
Fionn Malone <f.malone13@imperial.ac.uk>
2018-05-08 17:09:10 -0700

    Find interpolating vectors on subset of orbitals.

--- thc/ccode/src/interpolating_vectors.h
+++ thc/ccode/src/interpolating_vectors.h
@@ -12,16 +12,26 @@ namespace InterpolatingVectors
   class IVecs
   {
     public:
-      IVecs(nlohmann::json &input_file, ContextHandler::BlacsHandler &BH, std::vector<int> &interp_indxs);
+      IVecs(nlohmann::json &input_file, ContextHandler::BlacsHandler &BH, std::vector<int> &interp_indxs, bool half_rotated, bool append);
       void kernel(ContextHandler::BlacsHandler &BH);
+      void dump_qmcpack_data(int thc_cfac, int thc_half_cfac,
+                             ContextHandler::BlacsHandler &BH);
     private:
       void fft_vectors(ContextHandler::BlacsHandler &BH, DistributedMatrix::Matrix<std::complex<double> > &IVG,
                        DistributedMatrix::Matrix<std::complex<double> > &IVMG);
       void dump_thc_data(DistributedMatrix::Matrix<std::complex<double> > &IVG, DistributedMatrix::Matrix<std::complex<double> > &IVMG,
                          ContextHandler::BlacsHandler &BH);
+      void setup_CZt(std::vector<int> &interp_indxs, ContextHandler::BlacsHandler &BH);
+      void setup_CZt_half(std::vector<int> &interp_indxs, ContextHandler::BlacsHandler &BH);
+      void setup_CCt(std::vector<int> &interp_indxs, ContextHandler::BlacsHandler &BH);
+      void check_rank(ContextHandler::BlacsHandler &BH);
+      void setup_orbital_products(DistributedMatrix::Matrix<std::complex<double> > &Pua, std::vector<int> &interp_indxs, ContextHandler::BlacsHandler &BH, std::string aos, bool write, std::string prfx);
       DistributedMatrix::Matrix<std::complex<double> > CCt, CZt;
       std::string input_file, output_file;
-      int thc_cfac;
+      int thc_cfac, thc_half_cfac;
+      int nbasis;
+      bool half_rotate;
+      std::string prefix;
   };
 }
 #endif
